c314ddeadcb8e1e37cd7ceae146cee3cbe2688d0
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-19 16:10:55 -0700

    added global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <>
       <h1>Gobarber</h1>
-    </div>
+      <GlobalStyle />
+    </>
   );
 };
 
