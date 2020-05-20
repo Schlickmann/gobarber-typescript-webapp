1d0bd4c3c0e9f81e56612a4eb8e253c8b3584a58
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-20 09:03:05 -0700

    added unform to handle input forms

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logo from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -7,13 +8,16 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUn: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
   return (
     <Container>
       <Background />
       <Content>
         <img src={logo} alt="GoBarber Sign In" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Sign up for GoBarber</h1>
 
           <Input name="name" icon={FiUser} type="text" placeholder="Name" />
@@ -26,7 +30,7 @@ const SignUn: React.FC = () => {
           />
 
           <Button type="submit">Sign Up</Button>
-        </form>
+        </Form>
 
         <a href="/">
           <FiArrowLeft />
