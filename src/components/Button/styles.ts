adc82491328025b20525fcec2cc4748d61af8d2d
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-20 08:33:06 -0700

    moved button style to its onw component

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,3 +1,18 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.button``;
+export const Container = styled.button`
+  background-color: #ff9000;
+  color: #322e38;
+  border-radius: 10px;
+  border: 0;
+  padding: 1rem;
+  width: 100%;
+  font-weight: 600;
+  margin-top: 1rem;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: ${shade(0.2, '#ff9000')};
+  }
+`;
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -26,22 +26,6 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    button {
-      background-color: #ff9000;
-      color: #322e38;
-      border-radius: 10px;
-      border: 0;
-      padding: 1rem;
-      width: 100%;
-      font-weight: 600;
-      margin-top: 1rem;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background-color: ${shade(0.2, '#ff9000')};
-      }
-    }
-
     a {
       color: #f4ede8;
       display: block;
