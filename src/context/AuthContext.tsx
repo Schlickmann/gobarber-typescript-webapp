11fbad08dd1cefe699c3d3ed4c46e246dfd7893d
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-24 19:37:24 -0700

    getting token and user from session

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -17,7 +17,7 @@ const AuthProvider: React.FC = ({ children }) => {
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('/sessions', { email, password });
 
-    console.log(response.data);
+    const { token, user } = response.data;
   }, []);
 
   return (
