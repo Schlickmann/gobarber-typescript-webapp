94dfbf6969bc5df55d7c0426cad84fac25d1bfeb
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-28 18:36:59 -0700

    created app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import SignIn from './pages/SignIn';
+import Routes from './routes';
 import AppProvider from './context';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <>
+    <BrowserRouter>
       <AppProvider>
-        <SignIn />
+        <Routes />
       </AppProvider>
-
       <GlobalStyle />
-    </>
+    </BrowserRouter>
   );
 };
 
